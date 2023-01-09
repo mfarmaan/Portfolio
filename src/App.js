801a45c32199ec83f341d3bcfea1e2b0be47ae81
Mohhomad Farman <mohhomadfarman@gmail.com>
2023-01-10 00:52:18 +0530

    Revert "NEW CpMMIT"
    This reverts commit e3b453f66039987323dfb3ba3d1ccd378129d84e.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import './Media.css'
-import './index.css'
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-
-import styled, { css } from 'styled-components'
-
-
-import Layout from './Home/Layout';
-import LeftsideBlog from './assets/LeftsideBlog';
-import Readingblog from './component/Readingblog';
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<Layout />} />
-          <Route path="/about" element={<LeftsideBlog />} />
-          <Route path="/readingblog" element={<Readingblog />} />
-        </Routes>
-      </Router>
-
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
-
 
 export default App;
