8d283b995329fa7272cdf04fa16addf76f06b26a
Sunder <sunder@silstonegroup.com>
2023-02-17 01:13:18 +0530

    Blog.jsx chnages

--- src/component/Blog.jsx
+++ src/component/Blog.jsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Demo from "../img//demo_image-1-300x300.jpg";
-import LeftsideBlog from "../assets/LeftsideBlog";
-import Rightsidewiget from "../assets/Rightsidewiget";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper";
 import SwiperCore, { Pagination, Navigation } from "swiper/core";
@@ -12,26 +11,58 @@ import "swiper/css";
 SwiperCore.use([Pagination, Navigation]);
 
 function Blog() {
+  const [btnClicked, setBtnclicked] = useState(0);
+
+  const btnId = [
+    {
+      id: "1",
+      name: "Andriod Dev",
+    },
+    {
+      id: "2",
+      name: "Innovation",
+    },
+    {
+      id: "3",
+      name: "Design",
+    },
+    {
+      id: "4",
+      name: "web",
+    },
+  ];
+
+  const postdata = [
+    {
+      catagory: "Creative",
+      title: "The I tool that helps remote teams collaborate better",
+    },
+    {
+      catagory: "Inovation",
+      title: "The I tool that helps remote teams collaborate better",
+    },
+    {
+      catagory: "Design",
+      title: "The I tool that helps remote teams collaborate better",
+    },
+  ];
+  console.log(btnId);
   return (
     <>
-      <section className="my-5 top-section">
-        <div className="container">
+      <section className="my-5 top-section common-margin">
+        <div className="container top-section">
           <div className="row mobile-reverse">
             <div className="col-md-7">
               <div className="backgroung_post">
                 <Swiper
-                  pagination={{
-                    clickable: true,
-                  }}
-                  navigation={true}
                   className="mySwiper"
                   spaceBetween={30}
                   slidesPerView={2}
                   direction="vertical"
                   // loop={true}
-                  speed={500}
+                  speed={1500}
                   autoplay={{
-                    delay: 2000,
+                    delay: 1000,
                     disableOnInteraction: false,
                   }}
                   breakpoints={{
@@ -54,7 +85,7 @@ function Blog() {
                   modules={[Autoplay]}
                 >
                   <SwiperSlide>
-                    <div className="Post__card">
+                    <div className="Post__card pointer">
                       <div className="post_id_slider">
                         <span>01</span>
                       </div>
@@ -76,7 +107,7 @@ function Blog() {
                     </div>
                   </SwiperSlide>
                   <SwiperSlide>
-                    <div className="Post__card">
+                    <div className="Post__card pointer">
                       <div className="post_id_slider">
                         <span>01</span>
                       </div>
@@ -98,7 +129,7 @@ function Blog() {
                     </div>
                   </SwiperSlide>
                   <SwiperSlide>
-                    <div className="Post__card">
+                    <div className="Post__card pointer">
                       <div className="post_id_slider">
                         <span>01</span>
                       </div>
@@ -212,6 +243,114 @@ function Blog() {
                 <img src={Demo} className="img-fluid" alt="" />
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-gray-section common-margin">
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <div className="treaning_section">
+                <h2>Trending Topics</h2>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="d-flex treaning_section justify-content-end pointer">
+                <p className="m-0">See All Topics</p>
+              </div>
+            </div>
+          </div>
+          <div className="row ct-margin">
+            <div className="col-xl-2 col-lg-4 col-md-4 col-sm-6 mb-3 pointer">
+              <div className="catagaroy-img ">
+                <img src={Demo} alt="" />
+                <div className="catagaroy_text">
+                  <p>Red Color</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-2 col-lg-4 col-md-4 col-sm-6 mb-3 pointer">
+              <div className="catagaroy-img ">
+                <img src={Demo} alt="" />
+                <div className="catagaroy_text">
+                  <p>Red Color</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-2 col-lg-4 col-md-4 col-sm-6 mb-3 pointer">
+              <div className="catagaroy-img ">
+                <img src={Demo} alt="" />
+                <div className="catagaroy_text">
+                  <p>Red Color</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-2 col-lg-4 col-md-4 col-sm-6 mb-3 pointer">
+              <div className="catagaroy-img ">
+                <img src={Demo} alt="" />
+                <div className="catagaroy_text">
+                  <p>Red Color</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-2 col-lg-4 col-md-4 col-sm-6 mb-3 pointer">
+              <div className="catagaroy-img ">
+                <img src={Demo} alt="" />
+                <div className="catagaroy_text">
+                  <p>Red Color</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-2 col-lg-4 col-md-4 col-sm-6 mb-3 pointer">
+              <div className="catagaroy-img ">
+                <img src={Demo} alt="" />
+                <div className="catagaroy_text">
+                  <p>Red Color</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="common-margin">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <div className="treaning_section common-margin-heading">
+                <h2>Innovation & Tech</h2>
+              </div>
+            </div>
+            <div className="col-12 common-margin-heading">
+              <div className="d-flex gap-3">
+                {btnId.map((data) => (
+                  <button
+                    id={data.id}
+                    onClick={() => setBtnclicked(data.id)}
+                    type="submit"
+                    className={
+                      !btnClicked ? "catagory_tabs " : "catagory_tabs active"
+                    }
+                  >
+                    {data.name}
+                  </button>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            {postdata.map((postItem) => (
+              <div className=" col-xl-4 col-md-4  mb-4">
+                <div className="posts__card">
+                  <img src={Demo} alt="" />
+                  <div className="post_details">
+                    <p>{postItem.catagory}</p>
+                    <h2>{postItem.title}</h2>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </section>
